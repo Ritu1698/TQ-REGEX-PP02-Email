6da2d3eeafa7bef61a99b8241e4a1b91b40f1fe8
Ritu1698 <biswasrituparna83@gmail.com>
2020-09-28 21:48:43 +0530

    [Rituparna]Added: Checking Optional Parts

--- src/com/bridgelabs/email/Regex.java
+++ src/com/bridgelabs/email/Regex.java
@@ -9,7 +9,7 @@ public class Regex {
     { 
   
 		System.out.println("Welcome to Email problem");
-        String regex = "^[a-zA-Z]+[.+-_]{0,1}[a-zA-Z0-9]*@[a-zA-Z]+.[(com)|(co)]+";
+        String regex = "^[a-zA-Z]+([._+-]{0,1}[a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[(com)|(co)|(net)]+(?:\\.[a-z]{2,}){0,1}$";
         
         String email = "abc.xyz@bridgelabz.co"; 
         String email1 = "abc.xyz@bridgelabz.co"; 
